463422028fbaacf8c08cae8c95781bef2419adc6
MarkSigma345 <kkomarov012@gmail.com>
2025-03-01 13:46:34 +0700

    Initial commit

--- logic.py
+++ logic.py
@@ -1,17 +1,23 @@
 from random import randint
 import requests
-
+import random
+import datetime
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
+        
+        self.hp = randint(200,400)
+        self.power = randint(30,60)
+
+        self.last_feed_time = datetime.now()
 
         self.pokemon_trainer = pokemon_trainer   
         self.level = 1
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+    
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -33,11 +39,12 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-
+        
+   
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}, Здоровье:{self.hp}, Сила:{self.power}"
 
     def level_up(self):
         self.level += 1  # Метод для повышения уровня
@@ -48,5 +55,50 @@ class Pokemon:
     def show_img(self):
         return self.img
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = random.randint(1,5)
+            if chance  == 1:
+                return "Покемон-волшебник применил щит в сражении"
+    
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+    def feed(self, feed_interval = 60, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = datetime.timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"       
+        
 
 
+
+
+class Wizard(Pokemon):
+    def feed(self):
+        return super().feed(hp_increase=20)
+    def info(self):
+        return 'У тебя покемон-волшебник \n\n' +super().info()
+        
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        superpower = random.randint(5,15)
+        self.power += superpower
+        result = super().attack(enemy)
+        self.power -= superpower
+        return result + f"\nБоец применил супер-атаку силой:{superpower} "
+        
+    def info(self):
+        return 'У тебя покемон-волшебник \n\n' +super().info()
+    
+    def feed(self):
+        return super().feed(hp_increase=10)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot 
 from config import token
-from logic import Pokemon
+from logic import Pokemon 
+from logic import Wizard
+from logic import Fighter
 
 bot = telebot.TeleBot(token) 
 
@@ -13,17 +15,32 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
     
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.info())
+
+if __name__ == '__main__':
+    wizard = Wizard("username1",1,1)
+    fighter = Fighter("username2",1,1)
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
+
 @bot.message_handler(commands=['feed'])
-def feed(message):
-    
-    username = message.from_user.username
-    if username in Pokemon.pokemons:
-        pokemon = Pokemon.pokemons[username]
-        pokemon.level_up()  # Вызываем метод для повышения уровня
-        bot.send_message(message.chat.id, f"Ты покормил покемона!  Теперь его уровень: {pokemon.level}") # Отправляем сообщение об уровне
+def feed_pok(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+         pok = Pokemon.pokemons[message.from_user.username]
+         response = pok.feed()
+         bot.send_message(message.chat.id, response)
     else:
-        bot.reply_to(message, "У тебя еще нет покемона! Используй /go чтобы создать.")
-    
+        bot.send_message(message.chat.id, 'У вас нет покемона!')
+
 
 bot.infinity_polling(none_stop=True)
 
